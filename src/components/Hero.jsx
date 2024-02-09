e18372a2c487226a08c02dac4cc7ae9dd6119bd1
GoatSKR <sitesh.raulo.pj@gmail.com>
2024-02-10 01:33:45 +0530

    hero added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
-export default function Hero(){
-    return(
-        <div className="text-3xl text-bold">
-            Hello
+export default function Hero() {
+    return (
+        <div className="bg-[#121211] h-auto">
+            <div className="  font-MATTERS  text-center py-[10%] px-[20%]">
+                <h1 className="text-7xl text-bold text-white">We design brands of today to build the vision of tomorrow.</h1>
+                <p className="text-lg px-[15%] text-gray-600">Doozy is a tiny design studio dedicated to redefining brand 
+                    identity for the new digital age. Our narrative is simple; envision</p>
+            </div>
         </div>
+
     )
 }
