e6e5f0b9def248b12243617342fa8f974ba7d523
GoatSKR <sitesh.raulo.pj@gmail.com>
2024-02-15 21:52:13 +0530

    logo email adjusted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,12 +21,13 @@ function Header() {
         <header className="fixed top-0 left-0 right-0 bg-transparent z-50" >
             <nav className="flex justify-center md:justify-between md:items-center py-4 px-8 md:px-16">
                 <div className='hidden md:block'>
-                    <div className="grid grid-cols-2 gap-1">
-                        <img className='h-12 w-12' src="/static/images/onestudio.png" alt="logo" />
+                    <div className="grid grid-cols-2 ">
+                        <img className="place-self-center h-14 w-14 " src="/static/images/onestudio.png" alt="logo" />
                         <a href="mailto:hi@sridhar.fyi" className="text-white hover:text-[#121211] hover:bg-[#e3f551] rounded-3xl bg-[#252525] bg-opacity-50 px-4 my-2 flex items-center justify-center">
                             hi@sridhar.fyi
                         </a>
                     </div>
+
                 </div>
 
                 <ul className="flex gap-1">
